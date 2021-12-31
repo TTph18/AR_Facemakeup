d7f2d7baee083fa24a29ee5d8b45034c89a4cfe9
TranThiHaPhuong <59615434+TTph18@users.noreply.github.com>
2021-12-31 18:31:42 +0700

    Change name

--- Assets/Scripts/FaceSwap.cs
+++ Assets/Scripts/FaceSwap.cs
@@ -12,7 +12,7 @@ public class FaceSwap : MonoBehaviour
     {
         faceManager = GetComponent<ARFaceManager>();
     }
-    public void SwitchCheek1()
+    public void SwitchCheek_Freckle()
     {
         foreach (ARFace face in faceManager.trackables)
         {
@@ -24,161 +24,161 @@ public class FaceSwap : MonoBehaviour
         //    faceMaterialIndex = 0;
         //}
     }
-    public void SwitchCheek2()
+    public void SwitchCheek_Coral()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[2];
         }
     }
-    public void SwitchCheek3()
+    public void SwitchCheek_Crimson()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[3];
         }
     }
-    public void SwitchCheek4()
+    public void SwitchCheek_Lavender()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[4];
         }
     }
-    public void SwitchCheek5()
+    public void SwitchCheek_LightPink()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[5];
         }
     }
-    public void SwitchEyeshadow1()
+    public void SwitchEyeshadow_SnowWhite()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[6];
         }
     }
-    public void SwitchEyeliner1()
+    public void SwitchEyeliner_CatEyes()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[7];
         }
     }
-    public void SwitchEyeliner2()
+    public void SwitchEyeliner_SmokeyEyes()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[8];
         }
     }
-    public void SwitchLip1()
+    public void SwitchLip_DarkRose()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[9];
         }
     }
-    public void SwitchLip2()
+    public void SwitchLip_PinkGlitter()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[10];
         }
     }
-    public void SwitchLip3()
+    public void SwitchLip_LightRose()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[11];
         }
     }
-    public void SwitchLip4()
+    public void SwitchLip_WhiteGlitter()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[12];
         }
     }
-    public void SwitchTattoo1()
+    public void SwitchTattoo_Carnival()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[13];
         }
     }
-    public void SwitchTattoo2()
+    public void SwitchTattoo_LoveFreckle()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[14];
         }
     }
-    public void SwitchTattoo3()
+    public void SwitchTattoo_Gothic()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[15];
         }
     }
-    public void SwitchTattoo4()
+    public void SwitchTattoo_Mustache()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[16];
         }
     }
-    public void SwitchTattoo5()
+    public void SwitchTattoo_CarnivalForehead()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[17];
         }
     }
-    public void SwitchEyeshadow2()
+    public void SwitchEyeshadow_Smokey()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[18];
         }
     }
-    public void SwitchEyeshadow3()
+    public void SwitchEyeshadow_Glittering()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[19];
         }
     }
-    public void SwitchEyeshadow4()
+    public void SwitchEyeshadow_Pink()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[20];
         }
     }
-    public void SwitchEyeshadow5()
+    public void SwitchEyeshadow_RockRoll()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[21];
         }
     }
-    public void SwitchEyeliner3()
+    public void SwitchEyeliner_SnowWhite()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[22];
         }
     }
-    public void SwitchFullMakeup1()
+    public void SwitchFullMakeup_SmokeyFreckle()
     {
         foreach (ARFace face in faceManager.trackables)
         {
             face.GetComponent<MeshRenderer>().material = faceMaterials[23];
         }
     }
-    public void SwitchFullMakeup2()
+    public void SwitchFullMakeup_Mature()
     {
         foreach (ARFace face in faceManager.trackables)
         {
